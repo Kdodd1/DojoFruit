0d0627804847f2fc3e08b6cc57d413494eb38c5c
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-02 08:39:14 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    date = datetime.now()
+    sumTotal = str(int(strawberry)+int(raspberry)+int(apple))
+    return render_template("checkout.html", sumTotal= sumTotal, date=date)
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
